79c717edbcb46704bf2c7c4360597b36283f9cb0
Tristan <trstn.shmkr@gmail.com>
2020-08-04 15:16:36 -0400

    enforce unique time index on startup

--- data_collection.py
+++ data_collection.py
@@ -35,6 +35,8 @@ def connect(ip):
 
 def run():
     db = connect(mongo_ip)
+    result = db.create_index([('dateandtime', pymongo.ASCENDING)], unique=True)
+    print(F"Creating Unique Time Index: {result}")
     while(True):
         post = getData(WEL_ip)
         try:
