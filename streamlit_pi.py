e446eb744927799b37657553ae35105fa0272d29
Tristan <trstn.shmkr@gmail.com>
2020-09-22 15:10:54 -0400

    using matplotlib for now

--- streamlit_pi.py
+++ streamlit_pi.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import altair as alt
+import matplotlib.pyplot as plt
+import datetime as dt
 import numpy as np
 import pandas as pd
 import sys
@@ -9,25 +11,24 @@ from WELServer import WELData
 
 # @st.cache(hash_funcs={WELData: id})
 def makeWEL():
-    return WELData(mongo_local=False,
-                   timerange=['-t', '12'])
+    return WELData(mongo_local=False)
 
 # Load data and melt into format for alt
 dat = makeWEL()
-dat.data = dat.data.reset_index()
+# dat.data = dat.data.reset_index()
 # dat.data
 
 nearestTime = alt.selection(type='single', nearest=True, on='mouseover',
                             fields=['dateandtime'], empty='none')
 
-def plotMainMonitor(vars):
+def plotMainMonitor(vars,
+                    status_plot=False):
     status_list=['aux_heat_b', 'heat_1_b', 'heat_2_b', 'rev_valve_b',
                  'TAH_fan_b', 'zone_1_b', 'zone_2_b', 'humid_b']
-
-    temp_source = dat.data.melt(id_vars='dateandtime',
+    temp_source = dat.data.reset_index()
+    temp_source = temp_source.melt(id_vars='dateandtime',
                            value_vars=vars,
                            var_name='label')
-    print(temp_source)
     lines = alt.Chart(temp_source).mark_line(interpolate='basis').encode(
         x=alt.X('dateandtime:T', axis=alt.Axis(title=None, labels=True)),
         y=alt.Y('value:Q', axis=alt.Axis(format='Q',
@@ -57,28 +58,57 @@ def plotMainMonitor(vars):
         nearestTime
     )
 
-    temp_plot = alt.layer(
+    plot = alt.layer(
         lines, selectors, points, text, rules
     )
 
-    stat_source = dat.data.melt(id_vars='dateandtime',
-                                   value_vars=status_list,
-                                   var_name='label')
-    stat_source.value = stat_source.value % 2
-    # stat_source
-    status_plot = alt.Chart(stat_source).mark_bar(width=7).encode(
-        alt.X('dateandtime:T'),
-        alt.Y('label'),
-        alt.FillOpacity('value:Q', legend=None),
-        alt.Opacity('value:Q', legend=None)
-    )
+    if status_plot:
+        stat_source = dat.data.melt(id_vars='dateandtime',
+                                       value_vars=status_list,
+                                       var_name='label')
+        stat_source.value = stat_source.value % 2
+        status_plot = alt.Chart(stat_source).mark_bar(width=7).encode(
+            alt.X('dateandtime:T'),
+            alt.Y('label'),
+            alt.FillOpacity('value:Q', legend=None),
+            alt.Opacity('value:Q', legend=None)
+        )
+        plot = plot & status_plot
 
-
-    plot = temp_plot & status_plot
     return plot
 
 
+def plotMainMonitor_pyplot(vars,
+                           timerange=None):
+    fig, axes = plt.subplots(3, 1,
+                             sharex=True,
+                             figsize=(9,9),
+                             gridspec_kw={'height_ratios': [1, 0.4, 0.4]})
+
+    dat.plotVar(vars,
+                statusmask='heat_1_b',
+                axes=axes[0])
+
+    dat.plotStatus(axes=axes[1])
+
+    full_range_delta = dat.timerange[1] - dat.timerange[0]
+    rolling_interval = round(np.clip(((full_range_delta.total_seconds() / 3600) / 4), 1, 24))
+    dat.plotVar([F"COP.rolling('{rolling_interval}H').mean()"],
+            yunits=F'COP {rolling_interval} Hr Mean',
+            axes=axes[2])
+    axes[2].get_legend().remove()
+    plt.subplots_adjust(hspace=0.05)
+
+    return fig
+
+
 st.title('Geothermal Monitoring')
+
+# slider_time = st.select_slider("Time Range",
+#                         min_value=dat.time_from_args(['-t', '72'])[0],
+#                         max_value=dat.time_from_args()[1],
+#                         value=dat.time_from_args(),
+#                         step=dt.timedelta(minutes=30))
 
 sensors = st.multiselect(" ",
                          list(dat.vars()),
@@ -90,6 +120,9 @@ sensors = st.multiselect(" ",
                           'living_T',
                           'trist_T',
                           'base_T'])
-temp = plotMainMonitor(sensors)
+# temp = plotMainMonitor(sensors)
+temp_pyplot = plotMainMonitor_pyplot(sensors)
 
-st.altair_chart(temp, use_container_width=True)
+
+# st.altair_chart(temp, use_container_width=True)
+st.pyplot(temp_pyplot)
