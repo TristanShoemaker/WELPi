0ff84e5107c1ea745125b66240c86393295a9301
Tristan <trstn.shmkr@gmail.com>
2020-10-12 17:27:03 -0400

    integrated async plot, changed messaging to new system

--- data_collection.py
+++ data_collection.py
@@ -3,10 +3,12 @@ from pymongo.errors import DuplicateKeyError
 import requests
 from requests.exceptions import ConnectionError
 import xmltodict
-from time import sleep
+import time
 from datetime import datetime as dt
 from dateutil import tz
 from astral import sun, LocationInfo
+from libmc import Client
+from streamlit_pi import streamPlot, message
 
 
 WEL_ip = '192.168.68.107'
@@ -22,9 +24,8 @@ def getData(ip):
     try:
         response = requests.get(url)
     except ConnectionError:
-        print("error in connecting to WEL, waiting 5 sec then trying again",
-              flush=True)
-        sleep(5)
+        message("Error in connecting to WEL, waiting 10 sec then trying again")
+        time.sleep(10)
         response = requests.get(url)
     # print(response.content)
     response_data = xmltodict.parse(response.content)['Devices']['Device']
@@ -36,9 +37,9 @@ def getData(ip):
         except ValueError:
             post[item['@Name']] = item['@Value']
     date = dt.strptime(post['Date'], "%m/%d/%Y")
-    time = dt.strptime(post['Time'], "%H:%M:%S").time()
+    timeStamp = dt.strptime(post['Time'], "%H:%M:%S").time()
     # print(time)
-    post['dateandtime'] = (dt.combine(date, time)
+    post['dateandtime'] = (dt.combine(date, timeStamp)
                            .replace(tzinfo=tz.gettz('EST')))
     # print(post['dateandtime'])
     post['dateandtime'] = post['dateandtime'].astimezone(db_tzone)
@@ -55,34 +56,54 @@ def getData(ip):
     return post
 
 
-def connect(ip):
+def asyncPlot(mc):
+    stp = streamPlot()
+    stp.makeWEL(['-t', '12'])
+    plots = stp.plotAssembly()
+    mc_result = mc.set('plotKey', plots)
+    if not mc_result:
+        message("Plot failed to cache")
+        quit()
+
+
+def connectMongo(ip):
     address = "mongodb://" + ip + ":27017"
     client = MongoClient(address)
     db = client.WEL.data
+    message("Mongo Connected")
     return db
 
 
-def run():
+def connectMemCache():
+    mc = Client(['localhost'])
+    message("MemCache Connected")
+    return mc
+
+
+def main():
     print(F"\n Restarted {dt.now()} ...", flush=True)
-    db = connect(mongo_ip)
+    db = connectMongo(mongo_ip)
+    mc = connectMemCache()
     if "dateandtime_-1" not in list(db.index_information()):
         result = db.create_index([('dateandtime', DESCENDING)], unique=True)
         print(F"Creating Unique Time Index: {result}", flush=True)
     while True:
         post = getData(WEL_ip)
+        post_success = False
         try:
             post_id = db.insert_one(post).inserted_id
-            localtz = tz.gettz('America/New_York')
-            localtime = post['dateandtime'].astimezone(localtz)
-            print((F"UTC time: {post['dateandtime']} | "
-                   F"local time: {localtime} | "
-                   F"post_id: {post_id}"),
-                  flush=True)
+            message((F"UTC time: {post['dateandtime']} | "
+                     F"post_id: {post_id}"))
+            post_success = True
         except DuplicateKeyError:
             print(F"time key {post['dateandtime']} already in database",
                   flush=True)
 
-        sleep(30)
+        if post_success:
+            asyncPlot(mc)
+
+        time.sleep(30)
 
 
-run()
+if __name__ == "__main__":
+    main()
