4c80bca191051793e54db520fb3e06e1fbb2423b
TristanShoemaker <trstn.shmkr@gmail.com>
2020-10-12 10:09:53 -0400

    changed streamplot init

--- async_plot.py
+++ async_plot.py
@@ -1,18 +1,19 @@
 import time
 import libmc
-from streamlit_pi import streamPlot
+from streamlit_pi import streamPlot, message
 
 
 def run():
-    print(F"{time.strftime('%Y-%m-%d %H:%M')} : Async plot startup")
+    message("Async plot startup")
     mc = libmc.Client(['localhost'])
     while True:
-        stp = streamPlot(['-t', '12'])
+        stp = streamPlot()
+        stp.makeWEL(['-t', '12'])
         plots = stp.plotAssembly()
         mc_result = mc.set('plotKey', plots)
         if not mc_result:
             raise(F"{time.strftime('%Y-%m-%d %H:%M')} : Plot failed to Cache")
-        time.sleep(60)
+        time.sleep(59)
 
 
 run()
