49ff8c3bc02a11e24c8d875c3b97b1e1d9259920
Tristan <trstn.shmkr@gmail.com>
2020-11-10 16:08:11 -0500

    rewritten using asyncio, now logs a data point every 30 seconds

--- async_func.py
+++ async_func.py
@@ -2,8 +2,8 @@ import requests
 import xmltodict
 import time
 import platform
+import asyncio
 import datetime as dt
-from pymongo import DESCENDING
 from pymongo.errors import DuplicateKeyError
 from requests.exceptions import ConnectionError
 from pytz import timezone
@@ -14,22 +14,46 @@ from log_message import message
 from WELData import mongoConnect
 
 
-WEL_ip = '192.168.68.107'
-if platform.machine() == 'aarch64':
-    mongo_ip = 'localhost'
-elif platform.machine() == 'x86_64':
-    mongo_ip = '98.118.28.23'
+WEL_IP = '192.168.68.107'
+
+if platform.system() == 'Linux':
+    if platform.machine() == 'aarch64':
+        MONGO_IP = 'localhost'
+    elif platform.machine() == 'x86_64':
+        MONGO_IP = '98.118.28.23'
 elif platform.system() == 'Darwin':
-    mongo_ip = '192.168.68.101'
+    MONGO_IP = '192.168.68.101'
 else:
     raise("Unknown platform, can't choose mongoDB ip")
-loc = LocationInfo('Home', 'MA', 'America/New_York', 42.485557, -71.433445)
-to_tzone = timezone('America/New_York')
-db_tzone = timezone('UTC')
+
+LOC = LocationInfo('Home', 'MA', 'America/New_York', 42.485557, -71.433445)
+TO_TZONE = timezone('America/New_York')
+DB_TZONE = timezone('UTC')
 WEL_tzone = timezone('EST')
 
 
-def getWELData(ip):
+def connectMemCache():
+    ip = MONGO_IP + ":11211"
+    mc = Client([ip])
+    message("MemCache Connected", mssgType='ADMIN')
+    return mc
+
+
+def connectSense():
+    sn = Senseable()
+    if platform.system() == 'Linux':
+        path = "/home/ubuntu/WEL/WELPi/sense_info.txt"
+    elif platform.system() == 'Darwin':
+        path = "./sense_info.txt"
+
+    sense_info = open(path).read().strip().split()
+    sn.authenticate(*sense_info)
+    sn.rate_limit = 10
+    message("Sense Connected", mssgType='ADMIN')
+    return sn
+
+
+async def getWELData(ip):
     tic = time.time()
     url = "http://" + ip + ":5150/data.xml"
     try:
@@ -48,23 +72,18 @@ def getWELData(ip):
             post[item['@Name']] = float(item['@Value'])
         except ValueError:
             post[item['@Name']] = item['@Value']
-    date = dt.datetime.strptime(post['Date'], "%m/%d/%Y")
-    timeStamp = dt.datetime.strptime(post['Time'], "%H:%M:%S").time()
 
-    post['WELdateandtime'] = (dt.datetime.combine(date, timeStamp)
-                              .replace(tzinfo=WEL_tzone)
-                              .astimezone(db_tzone))
     post['dateandtime'] = (dt.datetime.now()
                            .replace(microsecond=0)
-                           .replace(tzinfo=to_tzone))
-    post['dateandtime'] = post['dateandtime'].astimezone(db_tzone)
+                           .replace(tzinfo=TO_TZONE))
+    post['dateandtime'] = post['dateandtime'].astimezone(DB_TZONE)
     del post['Date']
     del post['Time']
 
-    sunrise = sun.sunrise(loc.observer, date=post['dateandtime'].date(),
-                          tzinfo=to_tzone).astimezone(db_tzone)
-    sunset = sun.sunset(loc.observer, date=post['dateandtime'].date(),
-                        tzinfo=to_tzone).astimezone(db_tzone)
+    sunrise = sun.sunrise(LOC.observer, date=post['dateandtime'].date(),
+                          tzinfo=TO_TZONE).astimezone(DB_TZONE)
+    sunset = sun.sunset(LOC.observer, date=post['dateandtime'].date(),
+                        tzinfo=TO_TZONE).astimezone(DB_TZONE)
     post['daylight'] = ((post['dateandtime'] > sunrise)
                         and (post['dateandtime'] < sunset)) * 1
 
@@ -73,18 +92,19 @@ def getWELData(ip):
     return post
 
 
-def getRtlData(mc):
+async def getRtlData(mc):
     tic = time.time()
     post = mc.get('rtl')
     if post is None:
         message("RTL data not found in memCache", mssgType='WARNING')
+        return {}
     else:
         message([F"{'Getting RTL:': <20}", F"{time.time() - tic:.3f} s"],
                 mssgType='TIMING')
         return post
 
 
-def getSenseData(sn):
+async def getSenseData(sn):
     tic = time.time()
     sn.update_realtime()
     sense_post = sn.get_realtime()
@@ -102,79 +122,35 @@ def getSenseData(sn):
     return post
 
 
-# def asyncPlot(mc, timeKey):
-#     stp = StreamPlot()
-#     stp.makeWEL(['-t', '12'], force_refresh=True)
-#     plot_options = ['temp', 'pandw']
-#     for which in plot_options:
-#         message(F"Async plot {which}")
-#         plots = stp.plotAssembly(which=which)
-#         mc_result = mc.set(F"{which}PlotKey", {'plots': plots,
-#                                                'timeKey': timeKey})
-#         if not mc_result:
-#             message(F"❗{which} plot failed to cache❗")
-#             exit()
+async def send_post(db, post):
+    utc_time = post['dateandtime'].strftime('%Y-%m-%d %H:%M:%S')
+    try:
+        # post_id = db.insert_one(post).inserted_id
+        post_id = "fake"
+        message(F"Successful post @ UTC time: {utc_time}"
+                F" | post_id: {post_id}", mssgType='SUCCESS')
+    except DuplicateKeyError:
+        message("Tried to insert duplicate key "
+                F"{post['dateandtime'].strftime('%Y-%m-%d %H:%M:%S')}",
+                mssgType='WARNING')
 
 
-def connectMemCache():
-    mc = Client(['localhost'])
-    message("MemCache Connected", mssgType='ADMIN')
-    return mc
+async def main(interval, db, mc, sn):
+    while True:
+        then = time.time()
+        post = await getWELData(WEL_IP)
+        post.update(await getRtlData(mc))
+        post.update(await getSenseData(sn))
+        elapsed = time.time() - then
+        await asyncio.sleep(interval - elapsed)
+        await send_post(db, post)
 
 
-def connectSense():
-    sn = Senseable()
-    if platform.system() == 'Linux':
-        path = "/home/ubuntu/WEL/WELPi/sense_info.txt"
-    elif platform.system() == 'Darwin':
-        path = "./sense_info.txt"
-
-    sense_info = open(path).read().strip().split()
-    sn.authenticate(*sense_info)
-    sn.rate_limit = 20
-    message("Sense Connected", mssgType='ADMIN')
-    return sn
-
-
-def main():
+if __name__ == "__main__":
     message("\n    Restarted ...", mssgType='ADMIN')
     db = mongoConnect().data
     message("Mongo Connected", mssgType='ADMIN')
     mc = connectMemCache()
     sn = connectSense()
-    if "dateandtime_-1" not in list(db.index_information()):
-        result = db.create_index([('dateandtime', DESCENDING)], unique=True)
-        message(F"Creating Unique Time Index: {result}", mssgType='WARNING')
-    while True:
-        post = getWELData(WEL_ip)
-        last_post = db.find_one(sort=[('_id', DESCENDING)])
-        last_post = last_post['WELdateandtime'].replace(tzinfo=db_tzone)
-        utc_time = post['WELdateandtime'].strftime('%Y-%m-%d %H:%M:%S')
-        if post['WELdateandtime'] != last_post:
-            rtl_post = getRtlData(mc)
-            post.update(rtl_post)
 
-            try:
-                sense_post = getSenseData(sn)
-                post.update(sense_post)
-            except TypeError:
-                message("Empty sense data.", mssgType='ERROR')
-
-            try:
-                post_id = db.insert_one(post).inserted_id
-                message(F"Sucessful post @ WEL UTC time: {utc_time}"
-                        F" | post_id: {post_id}", mssgType='SUCCESS')
-            except DuplicateKeyError:
-                message("Tried to insert duplicate key "
-                        F"{post['dateandtime'].strftime('%Y-%m-%d %H:%M:%S')}",
-                        mssgType='WARNING')
-
-        else:
-            message(F"WEL UTC time: {utc_time} post already in database",
-                    mssgType='WARNING')
-
-        time.sleep(20)
-
-
-if __name__ == "__main__":
-    main()
+    asyncio.run(main(30, db, mc, sn))
