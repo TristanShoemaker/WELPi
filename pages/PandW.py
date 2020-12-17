7da4c1b7115d239007a2bca48055d25b435816d4
Tristan <trstn.shmkr@gmail.com>
2020-12-17 11:41:31 -0500

    changed power stack to temp monitor

--- pages/PandW.py
+++ pages/PandW.py
@@ -8,7 +8,9 @@ from log_message import message
 class PandW(StreamPlot):
     water_default = ['desup_T', 'desup_return_T', 'house_hot_T', 'tank_h2o_T',
                      'buderus_h2o_T']
-    pwr_default = ['TAH_W', 'HP_W', 'power_tot']
+    # pwr_default = ['TAH_W', 'HP_W', 'power_tot']
+    work_default = ['liqu_refrig_T', 'gas_refrig_T', 'loop_in_T', 'loop_out_T',
+                    'TAH_in_T', 'TAH_out_T']
     _sensor_groups = None
     plots = None
 
@@ -33,13 +35,13 @@ class PandW(StreamPlot):
 
     def _selects(self,
                  sensor_container):
-        pwr_sensors = sensor_container.multiselect("Power Sensors",
-                                                   self.sensor_list,
-                                                   self.pwr_default)
+        work_sensors = sensor_container.multiselect("Geo Working Sensors",
+                                                    self.sensor_list,
+                                                    self.work_default)
         water_sensors = sensor_container.multiselect("Water Sensors",
                                                      self.sensor_list,
                                                      self.water_default)
-        sensor_groups = [pwr_sensors, water_sensors]
+        sensor_groups = [work_sensors, water_sensors]
         for sensor_group in sensor_groups:
             while len(sensor_group) < 1:
                 st.warning('Please select at least one sensor per plot')
@@ -50,36 +52,28 @@ class PandW(StreamPlot):
     def _plots(self):
         tic = time.time()
         if self._sensor_groups is None:
-            self._sensor_groups = [self.pwr_default, self.water_default]
+            self._sensor_groups = [self.work_default, self.water_default]
         with st.spinner('Generating Plots'):
             plot = alt.vconcat(
                 self.plotStatus().properties(
                     width=self.def_width,
                     height=self.def_height * self.stat_height_mod
                 ),
-                self.plotPowerStack(['base_load_w', 'dehumidifier_w',
-                                     'geo_tot_w', 'solar_w'],
-                                    axis_label="Power / kW").properties(
+                self.plotMainMonitor(self._sensor_groups[0]).properties(
                     width=self.def_width,
                     height=self.def_height
                 ),
-                self.plotMainMonitor(self._sensor_groups[0],
-                                     axis_label="Power / kW",
-                                     ).properties(
-                    width=self.def_width,
-                    height=self.def_height * self.stat_height_mod
-                ),
-                self.plotMainMonitor(self._sensor_groups[1]).properties(
-                    width=self.def_width,
-                    height=self.def_height * self.pwr_height_mod
-                ),
                 self.plotMainMonitor('TAH_fpm',
                                      axis_label="Wind Speed / m/s",
-                                     height_mod=self.pwr_height_mod,
-                                     bottomPlot=True
+                                     height_mod=self.pwr_height_mod
                                      ).properties(
                     width=self.def_width,
                     height=self.def_height * self.stat_height_mod
+                ),
+                self.plotMainMonitor(self._sensor_groups[1],
+                                     bottomPlot=True).properties(
+                    width=self.def_width,
+                    height=self.def_height
                 ),
                 spacing=self.def_spacing
             ).resolve_scale(
